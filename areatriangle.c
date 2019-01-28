a281bbddff6f47f4726045f821646e5ba2833e27
vardhit123 <43814816+vardhit123@users.noreply.github.com>
2019-01-28 17:34:44 +0530

    Create areatriangle.c

---
+++ areatriangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+struct triangle
+{
+double area;
+}b,h,result;
+int main()
+{
+printf("enter the base of the triangle");
+scanf("%lf",&b.area);
+printf("enter the height of triangle");
+scanf("%lf",&h.area);
+result=0.5*b*h;
+printf("area of the given triangle is %lf",result.area);
+return 0;
+}
